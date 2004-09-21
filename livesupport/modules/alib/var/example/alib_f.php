752c50bd5ab108130f7c7d5696598fd113f8ce29
(no author) <(no author)@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2004-09-21 00:43:11 +0000

    This commit was manufactured by cvs2svn to create branch 'VENDOR'.

--- livesupport/modules/alib/var/example/alib_f.php
+++
@@ -1,4 +0,0 @@
-<?
-// $Id: alib_f.php,v 1.1 2004/07/23 00:22:13 tomas Exp $
-$dbc->disconnect();
-?>
--- livesupport/modules/alib/var/example/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $Id: index.php,v 1.1 2004/07/23 00:22:13 tomas Exp $
-
-header ("location: alibExLogin.php");
-die;
-
-?>
