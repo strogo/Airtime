ace1ba0104d18f5b53cbbab0ac0fa88f4c41f08d
maroy <maroy@cfc7b370-4200-0410-a6e3-cb6bdb053afe>
2006-02-13 20:07:55 +0000

    updated gstreamer from 0.8.10 to 0.10.3

--- livesupport/src/tools/gstreamer/gstreamer-0.10.3/bin/prepare.sh
+++ livesupport/src/tools/gstreamer/gstreamer-0.10.3/bin/prepare.sh
@@ -21,16 +21,19 @@
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #
-#   Author   : $Author$
-#   Version  : $Revision$
-#   Location : $URL$
+#   Author   : $Author: fgerlits $
+#   Version  : $Revision: 1842 $
+#   Location : $URL: svn+ssh://maroy@code.campware.org/home/svn/repo/livesupport/trunk/livesupport/src/tools/gstreamer/gstreamer-0.8.10/bin/prepare.sh $
 #-------------------------------------------------------------------------------                                                                                
 #-------------------------------------------------------------------------------
 # Run this script to prepare gstreamer to be configured and compiled.
 # To read more about gstreamer, see http://gstreamer.freedesktop.org/
 #-------------------------------------------------------------------------------
-product=gstreamer-0.8.10
-plugins=gst-plugins-0.8.10
+liboil=liboil-0.3.6
+gstreamer=gstreamer-0.10.3
+plugins_base=gst-plugins-base-0.10.3
+plugins_good=gst-plugins-good-0.10.2
+plugins_ugly=gst-plugins-ugly-0.10.1
 
 reldir=`dirname $0`/..
 basedir=`cd ${reldir}; pwd;`
@@ -38,8 +41,11 @@ bindir=${basedir}/bin
 etcdir=${basedir}/etc
 tmpdir=${basedir}/tmp
 
-gstreamer_tar=${basedir}/src/${product}.tar.bz2
-plugins_tar=${basedir}/src/${plugins}.tar.bz2
+liboil_tar=${basedir}/src/${liboil}.tar.gz
+gstreamer_tar=${basedir}/src/${gstreamer}.tar.bz2
+plugins_base_tar=${basedir}/src/${plugins_base}.tar.bz2
+plugins_good_tar=${basedir}/src/${plugins_good}.tar.bz2
+plugins_ugly_tar=${basedir}/src/${plugins_ugly}.tar.bz2
 
 mkdir -p ${tmpdir}
 
@@ -47,33 +53,37 @@ mkdir -p ${tmpdir}
 cp -r $bindir/install-sh $tmpdir
 
 cd ${tmpdir}
-if [ ! -d $product ]; then
-    tar xfj ${gstreamer_tar}
-    cd ${product}
+if [ ! -d $liboil ]; then
+    tar xfz ${liboil_tar}
+    cd ${liboil}
     # patch here
 fi
 
 cd ${tmpdir}
-if [ ! -d $plugins ]; then
-    tar xfj ${plugins_tar}
-    cd ${plugins}
-    # see bug report at http://bugzilla.gnome.org/show_bug.cgi?id=314146
-    # for details on the following patch
-    patch -p1 < ${etcdir}/adder-caps-property.patch
-    # see bug report at http://bugzilla.gnome.org/show_bug.cgi?id=309218
-    # for details on the following patch
-    patch -p1 < ${etcdir}/adder-duration-fix.patch
-    # see bug report at http://bugzilla.gnome.org/show_bug.cgi?id=315457
-    # for details on the following patch
-    patch -p1 < ${etcdir}/adder-query.patch
-    # see bug report at http://bugzilla.gnome.org/show_bug.cgi?id=308167
-    # for details on the following patch
-    patch -p1 < ${etcdir}/switch-fix.patch
-    # see bug report at http://bugzilla.gnome.org/show_bug.cgi?id=308619
-    # for details on the following patch
-    patch -p1 < ${etcdir}/id3demuxbin-pad-free-fix.patch
-    # see bug report at http://bugzilla.gnome.org/show_bug.cgi?id=308663
-    # for details on the following patch
-    patch -p1 < ${etcdir}/typefind-smil.patch
+if [ ! -d $gstreamer ]; then
+    tar xfj ${gstreamer_tar}
+    cd ${gstreamer}
+    # patch here
+fi
+
+cd ${tmpdir}
+if [ ! -d $plugins_base ]; then
+    tar xfj ${plugins_base_tar}
+    cd ${plugins_base}
+    # patch here
+fi
+
+cd ${tmpdir}
+if [ ! -d $plugins_good ]; then
+    tar xfj ${plugins_good_tar}
+    cd ${plugins_good}
+    # patch here
+fi
+
+cd ${tmpdir}
+if [ ! -d $plugins_ugly ]; then
+    tar xfj ${plugins_ugly_tar}
+    cd ${plugins_ugly}
+    # patch here
 fi
 
